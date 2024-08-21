66858b91e7bc3a8697583d931841b8933a45e4e8
GITSangWoo <dogcenta@gmail.com>
2024-08-21 18:03:17 +0900

    make chat program

---
+++ src/kchat/kafka/cchat.py
@@ -0,0 +1,37 @@
+from kafka import KafkaConsumer 
+from json import loads
+import time 
+from datetime import datetime 
+
+consumer = KafkaConsumer(
+        'chat', 
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit = True,
+        group_id = 'chat-group',
+        value_deserializer=lambda x: loads(x.decode('utf-8'))
+)
+
+
+print("채팅 프로그램 - 메시지 수신")
+print("메시지 대기 중 ....")
+
+try:
+    for m in consumer:
+        data= m.value
+        print(f"[FRIEND]:[{datetime.fromtimestamp(data['time'])}] {data['message']}")
+
+
+except KeyboardInterrupt:
+    print("채팅 종료")
+finally:
+    consumer.close()
+
+
+
+
+
+
+
+
+
---
+++ src/kchat/kafka/pchat.py
@@ -0,0 +1,34 @@
+from kafka import KafkaProducer
+import json
+import time
+
+p = KafkaProducer(
+    #TODO
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda x: json.dumps(x).encode('utf-8')
+)
+
+print("채팅 프로그램 - 메시지 발신자")
+print("메시지를 입력하세여.(종료시 'exit' 입력") 
+
+while True:
+    msg = input("YOU: ")
+    if msg.lower() == 'exit':
+        break
+
+    data = {'message' : msg, 'time':time.time()}
+    # TODO 보내기  
+    p.send('chat',value=data)
+    p.flush()
+
+
+
+
+
+
+
+
+
+
+
+ 
